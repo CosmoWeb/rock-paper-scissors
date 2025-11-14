96e01a5df19510f2c6d1ee1975715681fa09a95a
Cosmo <240465688+CosmoWeb@users.noreply.github.com>
2025-11-14 13:52:18 +0100

    Create function to get input from user

--- javascript.js
+++ javascript.js
@@ -1,21 +1,31 @@
 console.log("Hello World!");
 
 function getComputerChoice(randomNumber){
-    let choice;
+    let computerChoice;
     randomNumber = generateNumber();
 
     if(randomNumber === 1){
-        choice = "rock";
+      computerChoice = "rock";
     }else if(randomNumber === 2){
-        choice = "paper"
+        computerChoice = "paper"
     }else if(randomNumber === 3){
-        choice = "scissors";
+        computerChoice = "scissors";
     }
-    return console.log(choice);
+    return console.log(computerChoice);
 }
 
 function generateNumber(){
     return Math.floor(Math.random() * 3) +1;
 }
 
-getComputerChoice();
+function getHumanChoice(){
+    message = "Rock, paper or scissors?";
+    let humanChoice = prompt(message);
+    console.log(humanChoice);
+    return humanChoice.toLowerCase;
+}
+
+humanScore = 0;
+computerScore = 0;
+
+getHumanChoice();
